e8ac79a47a851dd97415e97561dca898528c20f5
alextanhongpin <alextan220990@gmail.com>
2023-07-03 00:23:37 +0800

    chore: update examples

--- examples/README.md
+++ examples/README.md
@@ -9,9 +9,17 @@ When an error is encounted, we just flip the switch to toggle the log level.
 However, the order of the logs printed will be reversed (this may not be so important, because usually you can just reverse the sort order in your logs monitoring tools).
 
 
+With error:
 ```bash
 ➜  examples git:(main) ✗ go run main.go
-time=2023-07-02T21:30:13.622+08:00 level=ERROR msg="failed to call Bar"
-time=2023-07-02T21:30:13.872+08:00 level=DEBUG msg="called Bar" elapsed=232ns
-time=2023-07-02T21:30:13.872+08:00 level=DEBUG msg="called Foo" elapsed=281ns
+time=2023-07-03T00:10:17.085+08:00 level=ERROR msg="failed to call Bar" req_id=cigq2q7ltaq0d1n2nle0
+time=2023-07-03T00:10:17.340+08:00 level=DEBUG msg="called Bar" req_id=cigq2q7ltaq0d1n2nle0 elapsed=342ns
+time=2023-07-03T00:10:17.340+08:00 level=DEBUG msg="called Foo" req_id=cigq2q7ltaq0d1n2nle0 elapsed=458ns
+```
+
+Without error:
+
+```bash
+➜  examples git:(main) ✗ go run main.go
+time=2023-07-03T00:10:19.632+08:00 level=INFO msg="called Bar" req_id=cigq2qvltaq0dcj5tat0
 ```
--- examples/main.go
+++ examples/main.go
@@ -1,43 +1,85 @@
 package main
 
 import (
+	"context"
 	"math/rand"
 	"os"
 	"time"
 
+	"github.com/rs/xid"
 	"golang.org/x/exp/slog"
 )
 
-// TODO: Pass this per request and improve way of toggling.
-var requestLevel = new(slog.LevelVar)
-
 func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: requestLevel,
-	}))
-	Foo(logger)
+	ctx := context.Background()
+	Foo(ctx, NewLogger())
 }
 
-func Foo(l *slog.Logger) {
+func Foo(ctx context.Context, l logger) {
 	start := time.Now()
-	defer l.Debug("called Foo", slog.Duration("elapsed", time.Since(start)))
+	defer l.DebugCtx(ctx, "called Foo", slog.Duration("elapsed", time.Since(start)))
 
 	time.Sleep(250 * time.Millisecond)
 
-	Bar(l)
+	Bar(ctx, l)
 }
 
-func Bar(l *slog.Logger) {
+func Bar(ctx context.Context, l logger) {
 	start := time.Now()
-	defer l.Debug("called Bar", slog.Duration("elapsed", time.Since(start)))
+	defer l.DebugCtx(ctx, "called Bar", slog.Duration("elapsed", time.Since(start)))
 
 	isErr := rand.Intn(2) < 1
 	if isErr {
-		l.Error("failed to call Bar")
-		requestLevel.Set(slog.LevelDebug)
+		l.ErrorCtx(ctx, "failed to call Bar")
 	} else {
-		l.Info("called Bar")
+		l.InfoCtx(ctx, "called Bar")
 	}
 
 	time.Sleep(250 * time.Millisecond)
+}
+
+type logger interface {
+	ErrorCtx(ctx context.Context, msg string, attrs ...slog.Attr)
+	DebugCtx(ctx context.Context, msg string, attrs ...slog.Attr)
+	InfoCtx(ctx context.Context, msg string, attrs ...slog.Attr)
+}
+
+type Logger struct {
+	*slog.LevelVar
+	*slog.Logger
+}
+
+func NewLogger() *Logger {
+	level := new(slog.LevelVar)
+	logger := slog.
+		New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: level,
+		})).
+		With(slog.String("req_id", xid.New().String()))
+
+	return &Logger{
+		LevelVar: level,
+		Logger:   logger,
+	}
+}
+
+func (l *Logger) DebugCtx(ctx context.Context, msg string, attrs ...slog.Attr) {
+	l.LogAttrs(ctx, slog.LevelDebug, msg, attrs...)
+}
+
+func (l *Logger) InfoCtx(ctx context.Context, msg string, attrs ...slog.Attr) {
+	l.LogAttrs(ctx, slog.LevelInfo, msg, attrs...)
+}
+
+func (l *Logger) ErrorCtx(ctx context.Context, msg string, attrs ...slog.Attr) {
+	l.LogAttrs(ctx, slog.LevelError, msg, attrs...)
+}
+
+func (l *Logger) LogAttrs(ctx context.Context, level slog.Level, msg string, attrs ...slog.Attr) {
+	if level == slog.LevelError {
+		// Enable up to DEBUG level.
+		l.LevelVar.Set(slog.LevelDebug)
+	}
+
+	l.Logger.LogAttrs(ctx, level, msg, attrs...)
 }
